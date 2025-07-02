f5dd094e3638a44f0f14d578364ffcaf05e62c7e
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-07-02 13:34:57 +0530

    update the schema

--- Models/users.js
+++ Models/users.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+  name: { type: String },
+  contact: { type: String },
+  email: { type: String },
+  landmark: { type: String },
+  city: { type: String },
+  pincode: { type: String },
+  state: { type: String }
+});
+
 const userSchema = new mongoose.Schema({
   firstName: { type: String },
   lastName: { type: String },
   email: { type: String },
   businessType: { type: String },
-  address: { type: String },
+  addresses: [addressSchema],
   contact: { type: String, required: true, unique: true },
   otp: { type: String },
   otpExpires: { type: Date}
