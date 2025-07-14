ab0b5dc2c51b9d1f8aefcd588d4f7dff5aa612e1
kantiboyinaAjay <ajaykantiboyina12389@gmail.com>
2025-07-14 15:31:56 +0530

    add product controller

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const Router = express.Router();
 
-const { getUpload } = require('../Controller/utils/upload');
 const controller_auth = require('../Controller/auth');
 const controller_order = require('../Controller/order');
 const controller_product = require('../Controller/product');
-const controller_image = require('../Controller/image');
+const upload = require('../Controller/utils/upload');
 
 //Auth
 Router.post('/sendOTP', controller_auth.send_otp);
@@ -21,22 +20,9 @@ Router.patch('/updateStatus/:id', controller_order.updateOrderStatus);
 Router.post('/createOrder', controller_order.createOrder);
 
 //product
-// Router.post('/createProduct', getUpload().single("image"), controller_product.createProduct);
-
-Router.post("/createProduct", (req, res, next) => {
-  const upload = getUpload();
-  upload.single("image")(req, res, function (err) {
-    if (err) {
-      return res.status(500).json({ message: "Upload failed", error: err.message });
-    }
-    controller_product.createProduct(req, res);
-  });
-});
-
+Router.post('/createProduct', upload.single('image'), controller_product.createProduct);
 Router.get("/getAllProducts", controller_product.getAllProducts);
-Router.patch('/updateProduct/:id', controller_product.updateProduct);
-
-//image
-Router.get('/image/:filename', controller_image.getImageByFilename);
+Router.patch('/updateProduct/:id', upload.single('image'), controller_product.updateProduct);
+Router.delete('/deleteProduct/:id', controller_product.deleteProduct);
 
 module.exports = Router;
