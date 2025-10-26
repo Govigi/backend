0bb47552bff0b4045724d2ae15f6c0ea03849fa2
santoshaymar9322 <santoshaymar9322@gmail.com>
2025-10-26 11:11:57 +0530

    Migration(express -> nest): completed env and mongo setup

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersModule } from './customers/customers.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { PaymentsModule } from './payments/payments.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    CustomersModule,
+    ProductsModule,
+    OrdersModule,
+    AuthModule,
+    UsersModule,
+    InventoryModule,
+    PaymentsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- nest-api/src/app.service.ts
+++ nest-api/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private configService: ConfigService) {}
+  getHello(): string | number {
+    console.log('running at', this.configService.get('PORT'));
+    return this.configService.get<number>('PORT') || 'hello';
   }
 }
